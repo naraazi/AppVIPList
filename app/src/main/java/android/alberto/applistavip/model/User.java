694c43e00e28b29a2dc9e8c69c7d710346c050c4
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2023-12-08 09:56:18 -0300

    Lógica dos botões adicionada

--- app/src/main/java/android/alberto/applistavip/model/User.java
+++ app/src/main/java/android/alberto/applistavip/model/User.java
@@ -1,24 +1,24 @@
 package android.alberto.applistavip.model;
 
 public class User {
-    private String firstName;
+    private String firstname;
     private String surname;
     private String intendedCourse;
     private String phoneNumber;
 
     public User(String firstName, String surname, String intendedCourse, String phoneNumber) {
-        this.firstName = firstName;
+        this.firstname = firstName;
         this.surname = surname;
         this.intendedCourse = intendedCourse;
         this.phoneNumber = phoneNumber;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getFirstname() {
+        return firstname;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
     }
 
     public String getSurname() {
@@ -48,7 +48,7 @@ public class User {
     @Override
     public String toString() {
         return "User{" +
-                "firstName='" + firstName + '\'' +
+                "firstName='" + firstname + '\'' +
                 ", surname='" + surname + '\'' +
                 ", intendedCourse='" + intendedCourse + '\'' +
                 ", phoneNumber='" + phoneNumber + '\'' +
--- app/src/main/java/android/alberto/applistavip/view/MainActivity.java
+++ app/src/main/java/android/alberto/applistavip/view/MainActivity.java
@@ -3,14 +3,65 @@ package android.alberto.applistavip.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.alberto.applistavip.R;
+import android.alberto.applistavip.model.User;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    User user = new User(
+            "Lorenzo",
+            "Alberto",
+            "Computing",
+            "93 991853287"
+    );
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //CODE HERE, GODDAMMIT
+        EditText editTextFirstname = findViewById(R.id.editTextFirstname);
+        EditText editTextSurname = findViewById(R.id.editTextSurname);
+        EditText editTextIntendedCourse = findViewById(R.id.editTextIntendedCourse);
+        EditText editTextContactNum = findViewById(R.id.editTextContactNum);
+        Button btnClear = findViewById(R.id.btnClear);
+        Button btnSave = findViewById(R.id.btnSave);
+        Button btnSubmit = findViewById(R.id.btnSubmit);
+
+        // editTextFirstName.setText(user.getFirstName()); -- showed in the screen
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextFirstname.setText("");
+                editTextSurname.setText("");
+                editTextIntendedCourse.setText("");
+                editTextContactNum.setText("");
+            }
+        });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Finalizado", Toast.LENGTH_LONG).show();
+
+                finish();
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user.setFirstname(editTextFirstname.getText().toString());
+                user.setSurname(editTextSurname.getText().toString());
+                user.setIntendedCourse(editTextIntendedCourse.getText().toString());
+                user.setPhoneNumber(editTextContactNum.getText().toString());
+
+                Toast.makeText(MainActivity.this, "Resposta salva: " + user.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
