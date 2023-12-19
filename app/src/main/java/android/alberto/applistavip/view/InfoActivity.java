a63d6b414d65a5fd3a684f9752633dd4beb36b3f
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2023-12-19 09:26:42 -0300

    "InfoActivity" para visualização criada

---
+++ app/src/main/java/android/alberto/applistavip/view/InfoActivity.java
@@ -0,0 +1,15 @@
+package android.alberto.applistavip.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.alberto.applistavip.R;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
